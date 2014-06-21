844a1040e15f78c56501b81e1336cf02ffbd4337
Jim Schubert <james.schubert@gmail.com>
2014-06-21 19:43:57 -0400

    Init with yo angular

---
+++ .tmp/concat/scripts/scripts.js
@@ -0,0 +1,24 @@
+'use strict';
+angular.module('jimschubertusApp', [
+  'ngSanitize',
+  'ngRoute'
+]).config([
+  '$routeProvider',
+  function ($routeProvider) {
+    $routeProvider.when('/', {
+      templateUrl: 'views/main.html',
+      controller: 'MainCtrl'
+    }).otherwise({ redirectTo: '/' });
+  }
+]);
+'use strict';
+angular.module('jimschubertusApp').controller('MainCtrl', [
+  '$scope',
+  function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  }
+]);
